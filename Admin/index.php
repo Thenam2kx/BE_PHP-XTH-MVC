41c8ca295cd1679dfaa49d887d4c7d2983934959
Thenam2kx <thenam2kx@gmail.com>
2024-06-03 16:00:19 +0700

    upload create user

--- Admin/index.php
+++ Admin/index.php
@@ -1,19 +1,23 @@
 <?php
 include './src/Controllers/ProductCtrl.php';
+include './src/Controllers/UserCtrl.php';
 include './src/Includes/Helper.php';
 
 $url = isset($_GET['action']) ? $_GET['action'] : 'Home';
 
 
 $ProductCtrl = new ProductCtrl();
+$UserCtrl = new UserCtrl();
 
 switch ($url) {
   case 'Home':
     $ProductCtrl->ShowALlPrd();
     break;
+
   case 'CreatePrd':
     $ProductCtrl->CreatePrd();
     break;
+
   case 'UpdatePrd':
     $id = $_GET['id'];
     $ProductCtrl->UpdatePrd($id);
@@ -22,6 +26,14 @@ switch ($url) {
   case 'DeletePrd':
     $id = $_GET['id'];
     $ProductCtrl->DeletePrd($id);
+    break;
+    
+  case 'User':
+    $UserCtrl->ShowAllUser();
+    break;
+    
+  case 'CreateUser':
+    $UserCtrl->CreateUser();
     break;
   
   default:
---
+++ Admin/src/Controllers/UserCtrl.php
@@ -0,0 +1,55 @@
+<?php
+  include './src/Models/UserMdl.php';
+
+  class UserCtrl {
+    public $UserMdl;
+
+    public function __construct() {
+      $this->UserMdl = new UserMdl();
+    }
+
+    function ShowAllUser () {
+      $users = $this->UserMdl->GetAllUser();
+      include './src/Views/UserView.php';
+    }
+
+    function CreateUser () {
+      $roles = $this->UserMdl->GetAllRole();
+      include './src/Views/CreateUserView.php';
+
+      if (isset($_POST['CreateUser'])) {
+        $RoleID = $_POST['userRole'];
+        $FullName = $_POST['userName'];
+        $Email = $_POST['userEmail'];
+        $Phone = $_POST['userPhone'];
+        $Gender = $_POST['userGender'];
+        $Password = $_POST['userPass'];
+        // echo 'roleid: '.$RoleID.' /fullnane '.$FullName.' /email: '. $Email.' / Phone: '. $Phone.' / Gendeer: '. $Gender.' / pass '. $Password;
+        // die();
+        $this->UserMdl->AddUser($RoleID, $FullName, $Email, $Phone, $Gender, $Password);
+      }
+    }
+
+  //   function UpdatePrd ($id) {
+  //     $categorys = $this->ProductMdl->GetAllCate();
+  //     $infoPrd = $this->ProductMdl->GetOnePrd($id);
+  //     include './src/Views/UpdatePrdView.php';
+
+  //     if (isset($_POST['UpdatePrd'])) {
+  //       $productName = $_POST['productName'];
+  //       $productCategory = $_POST['productCategory'];
+  //       $productPrice = $_POST['productPrice'];
+  //       $productDescription = $_POST['productDescription'];
+  //       $CreateAt = date('y-m-d');
+  //       $Thumbnail = null;
+  //       $this->ProductMdl->UpdatePrd($productCategory, $productName, $Thumbnail, $productPrice, $CreateAt, $productDescription, $id);
+  //       // exit(header("location: ?action=Home"));
+  //     }
+      
+  //   }
+
+  //   function DeletePrd ($id) {
+  //       $this->ProductMdl->DeletePrdMdl($id);
+  //       header("location:?action=Home");
+  //   }
+  }
